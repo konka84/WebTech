7096d2e74986b8e675818cc68e62c8734f602476
konka <1784konka@gmail.com>
2021-10-01 22:58:26 +0600

    webtech

---
+++ app/models/orders.js
@@ -0,0 +1,25 @@
+const { Double } = require('mongodb');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var orderSchema = mongoose.Schema({
+    username: String,
+    contact: String,
+    items: Array,
+    specialInstructions: String,
+    specialLocations: String,
+    selectedShop: String,
+    selectedShop_id: String,
+    favorited: Boolean,
+    date: String,
+    time: String,
+    timeUntilArrival: String,
+    secondsUntilArrival: Number,
+    timeSelectedForPickup: String,
+    expectedPickupTime: String,
+    completed: Boolean,
+});
+
+var Orders = mongoose.model('Order', orderSchema);
+
+module.exports = Orders;
